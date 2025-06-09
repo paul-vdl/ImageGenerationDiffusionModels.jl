d83cfa2f5417df143da7480509ab5e70ea8c2b58
vidalp <paul.vidal@etu.ec-lyon.fr>
2025-06-09 11:43:53 +0200

    apply_noise

--- src/MyPackage.jl
+++ src/MyPackage.jl
@@ -19,8 +19,21 @@ function generate_grid()
                j*img_size[2]+1:(j+1)*img_size[2]] .= first64[:, :, idx]
     end
 
-    canvas = clamp01.(canvas)
-    save("grille.png", colorview(Gray, canvas))
+    canvas2 = clamp01.(canvas)
+    save("grille.png", colorview(Gray, canvas2))
+    return canvas
+end
+
+function apply_noise(img; num_noise_steps = 500, beta_min = 0.0001, beta_max = 0.02)
+    variance_schedule = beta_min : 1 / num_noise_steps : beta_max #defines how the model adds noise to the image
+    epsilon = randn(size(img)) #gaussian noise
+
+    for beta in variance_schedule #add noise gradually
+    img = sqrt(1-beta) .* img + sqrt(beta) .* epsilon
+    end
+    
+    canvas = clamp01.(img) #save the image in .png
+    save("noisy_grid.png", colorview(Gray, canvas)) 
 end
 
 end
