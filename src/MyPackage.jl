26ed11402efee466633651d707fe40c92e0fd4e6
Nada <nbraham65@gmail.com>
2025-06-22 17:45:45 +0200

    first version of the U-Net neural Network

---
+++ src/MyPackage.jl
@@ -0,0 +1,152 @@
+module MyPackage
+
+using MAT
+using Images
+using FileIO
+
+using Flux
+using NNlib: pad
+using Statistics: mean
+
+"""
+    function generate_grid()
+
+Loads the digits data and generates grid
+"""
+function generate_grid()
+    data = matread(joinpath(@__DIR__, "..", "SyntheticImages500.mat"))
+    raw = data["syntheticImages"]        
+    images = reshape(raw, 32, 32, 500)     
+
+    first64 = images[:, :, 1:64]
+    img_size = size(first64, 1), size(first64, 2)
+    canvas = zeros(Float32, 8 * img_size[1], 8 * img_size[2])
+
+    for i in 0:7, j in 0:7
+        idx = i * 8 + j + 1
+        canvas[i*img_size[1]+1:(i+1)*img_size[1],
+               j*img_size[2]+1:(j+1)*img_size[2]] .= first64[:, :, idx]
+    end
+
+    canvas2 = clamp01.(canvas)
+    save("grid.png", colorview(Gray, canvas2))
+    return canvas
+end
+
+"""
+    function apply_noise(img; num_noise_steps = 500, beta_min = 0.0001, beta_max = 0.02)
+
+Applies forward-noise to an image
+This function adds Gaussian noise to an image during multiple steps, which corresponds to the forward process in diffusion models.
+
+#Arguments
+- 'img' : The input image
+- 'num_noise_steps' : number of steps over which noise should be added to the image (500 by default).
+- 'beta_min': Minimum beta value (0.0001 by default)
+- 'beta_max': Maximum beta value (0.02 by default)
+
+#Returns
+- An image with noise
+
+"""
+function apply_noise(img; num_noise_steps = 500, beta_min = 0.0001, beta_max = 0.02)
+    variance_schedule = beta_min : 1 / num_noise_steps : beta_max #the way the model adds noise to the image
+    epsilon = randn(size(img)) #gaussian noise
+
+    for beta in variance_schedule 
+    img = sqrt(1-beta) .* img + sqrt(beta) .* epsilon
+    end
+    
+    canvas = clamp01.(img)
+    save("noisy_img.png", colorview(Gray, canvas)) 
+    return img
+end
+
+function sinusoidal_embedding(t::Vector{Float32}, dim::Int)
+    half_dim = div(dim, 2)
+    emb = log(10000.0) / (half_dim - 1)
+    emb = exp.((-emb) .* (0:half_dim - 1))
+    emb = t .* emb'
+    emb = hcat(sin.(emb), cos.(emb))
+    return emb
+end
+
+function pad_or_crop(x, ref)
+    _, _, h1, w1 = size(x)
+    _, _, h2, w2 = size(ref)
+    pad_h = max(0, h2 - h1)
+    pad_w = max(0, w2 - w1)
+    x = pad(x, (0,0), (0,0), (pad_h÷2, pad_h - pad_h÷2), (pad_w÷2, pad_w - pad_w÷2))
+    return x[:, :, 1:h2, 1:w2]
+end
+
+function down_block(in_ch, out_ch, time_dim)
+    conv1 = Chain(Conv((3,3), in_ch => out_ch, pad=1), BatchNorm(out_ch), relu)
+    conv2 = Chain(Conv((3,3), out_ch => out_ch, pad=1), BatchNorm(out_ch), relu)
+    downsample = Conv((4,4), out_ch => out_ch, stride=2, pad=1)
+    time_mlp = Dense(time_dim, out_ch)
+
+    return (x, t_emb) -> begin
+        h = conv1(x)
+        t_proj = reshape(relu(time_mlp(t_emb)), :, 1, 1, size(x)[end])
+        h = h .+ permutedims(t_proj, (4,1,2,3))
+        h = conv2(h)
+        return downsample(h), h
+    end
+end
+
+function up_block(in_ch, out_ch, time_dim)
+    upsample = ConvTranspose((4,4), in_ch => in_ch, stride=2, pad=1)
+    conv1 = Chain(Conv((3,3), in_ch + div(in_ch,2) => out_ch, pad=1), BatchNorm(out_ch), relu)
+    conv2 = Chain(Conv((3,3), out_ch => out_ch, pad=1), BatchNorm(out_ch), relu)
+    time_mlp = Dense(time_dim, out_ch)
+
+    return (x, skip, t_emb) -> begin
+        x = upsample(x)
+        x = pad_or_crop(x, skip)
+        x = cat(x, skip; dims=1)
+        h = conv1(x)
+        t_proj = reshape(relu(time_mlp(t_emb)), :, 1, 1, size(x)[end])
+        h = h .+ permutedims(t_proj, (4,1,2,3))
+        return conv2(h)
+    end
+end
+
+
+function build_unet(in_ch::Int=1, out_ch::Int=1, time_dim::Int=256)
+    conv0 = Conv((3,3), in_ch => 128, pad=1)
+
+    down1 = down_block(128, 256, time_dim)
+    down2 = down_block(256, 512, time_dim)
+    down3 = down_block(512, 1024, time_dim)
+
+    bottleneck = Chain(
+        Conv((3,3), 1024 => 1024, pad=1),
+        BatchNorm(1024),
+        relu,
+        Conv((3,3), 1024 => 1024, pad=1),
+        BatchNorm(1024),
+        relu
+    )
+
+    up1 = up_block(1024, 512, time_dim)
+    up2 = up_block(512, 256, time_dim)
+    up3 = up_block(256, 128, time_dim)
+
+    final = Conv((1,1), 128 => out_ch)
+
+    return (x, t_vec) -> begin
+        t_emb = sinusoidal_embedding(t_vec, time_dim)
+        x0 = conv0(x)
+        x1, skip1 = down1(x0, t_emb)
+        x2, skip2 = down2(x1, t_emb)
+        x3, skip3 = down3(x2, t_emb)
+        x4 = bottleneck(x3)
+        x = up1(x4, skip3, t_emb)
+        x = up2(x, skip2, t_emb)
+        x = up3(x, skip1, t_emb)
+        return final(x)
+    end
+end
+
+end
