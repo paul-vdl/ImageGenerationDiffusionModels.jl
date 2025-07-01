b0fc13fd0c64d2fdf9438ebda04fd30043e642c2
Nada <nbraham65@gmail.com>
2025-07-01 10:59:19 +0200

    another try **2

--- src/MyPackage.jl
+++ src/MyPackage.jl
@@ -6,14 +6,13 @@ using Images
 using FileIO
 using Statistics
 using Random
-using Flux: mse, gradient, DataLoader, params, setup, update!
+using Flux: mse, DataLoader, params, ADAM, gradient, update!
 using Printf
 
 function load_synthetic_images(path="SyntheticImages500.mat")
     data = matread(path)
     raw = data["syntheticImages"]
-    images = Float32.(reshape(raw, 32, 32, 1, :) ./ 255)
-    return images
+    Float32.(reshape(raw, 32, 32, 1, :) ./ 255)
 end
 
 function sinusoidal_embedding(t::Vector{Float32}, dim::Int)
@@ -21,7 +20,7 @@ function sinusoidal_embedding(t::Vector{Float32}, dim::Int)
     emb = log(10000.0f0) / (half_dim - 1)
     freqs = Float32.(exp.(-emb .* (0:half_dim - 1)))
     angles = t .* freqs'
-    return Float32.(hcat(sin.(angles), cos.(angles)))
+    Float32.(hcat(sin.(angles), cos.(angles)))
 end
 
 struct UNet
@@ -47,10 +46,10 @@ function (m::UNet)(x, t_vec)
     x1 = relu(m.conv0(x))
     x2 = m.down1(x1)
     x3 = m.down2(x2)
-    x3 = x3 .+ t_proj
+    x3 = x3 .+ t_proj  # Non-mutating addition to avoid AD errors
     x = m.up1(x3)
     x = m.up2(x)
-    return m.final(x)
+    m.final(x)
 end
 
 function build_unet(in_ch=1, out_ch=1, time_dim=128)
@@ -70,37 +69,45 @@ function apply_noise(img; beta_min=0.0001, beta_max=0.02, steps=500)
     beta = beta_min + (beta_max - beta_min) * t / steps
     ε = randn(Float32, size(img))
     noisy = sqrt(1 - beta) * img .+ sqrt(beta) * ε
-    return noisy, ε, Float32(t) / steps
+    noisy, ε, Float32(t) / steps
 end
 
 loss_fn(model, x, t, noise) = mse(model(x, t), noise)
 
-function train!(model, images; batch_size=32, epochs=3, η=1e-3)
+function train_model!(model, images; batch_size=32, epochs=3, η=1e-3)
     opt = ADAM(η)
-    state = setup(opt, model)
+    ps = params(model)
+    state = Flux.setup(opt, ps)
     loader = DataLoader(images, batchsize=batch_size, shuffle=true)
 
     for epoch in 1:epochs
-        total = 0f0
+        total_loss = 0f0
         n = 0
+
         for batch in loader
             bsz = size(batch)[end]
-            noisy = similar(batch)
-            noise = similar(batch)
+
+            noisy_list = Vector{Array{Float32,3}}(undef, bsz)
+            noise_list = Vector{Array{Float32,3}}(undef, bsz)
             t_vec = zeros(Float32, bsz)
+
             for i in 1:bsz
-                noisy_i, eps_i, t_i = apply_noise(batch[:,:,:,i])
-                noisy[:,:,:,i] = noisy_i
-                noise[:,:,:,i] = eps_i
+                noisy_i, noise_i, t_i = apply_noise(batch[:,:,:,i])
+                noisy_list[i] = noisy_i
+                noise_list[i] = noise_i
                 t_vec[i] = t_i
             end
 
-            grads = gradient(model -> loss_fn(model, noisy, t_vec, noise), model)
-            update!(state, model, grads)
-            total += loss_fn(model, noisy, t_vec, noise)
+            noisy_batch = cat(noisy_list..., dims=4)
+            noise_batch = cat(noise_list..., dims=4)
+
+            grads = gradient(() -> loss_fn(model, noisy_batch, t_vec, noise_batch), ps)
+            update!(state, ps, grads)
+            total_loss += loss_fn(model, noisy_batch, t_vec, noise_batch)
             n += 1
         end
-        @printf("Epoch %d | Loss = %.6f\n", epoch, total / n)
+
+        @printf("Epoch %d | Loss = %.6f\n", epoch, total_loss / n)
     end
 end
 
@@ -112,17 +119,31 @@ function sample_image(model, img_shape=(32,32,1); steps=500, beta_min=0.0001, be
         pred = model(reshape(x, 32, 32, 1, 1), [t])[:, :, :, 1]
         x = (x .- sqrt(beta) * pred) ./ sqrt(1 - beta)
     end
-    return clamp01.(x)
+    clamp01.(x)
 end
 
 function run_training()
     images = load_synthetic_images()
     model = build_unet()
+
+    # Initialize all parameters by running a dummy forward pass
+    dummy_t = fill(0.0f0, 1)        # batch size 1 time scalar
+    dummy_x = rand(Float32, 32, 32, 1, 1)
+    model(dummy_x, dummy_t)
+
     println("[INFO] Training start...")
-    train!(model, images)
+    train_model!(model, images)
     println("[INFO] Sampling...")
     img = sample_image(model)
     save("sample.png", colorview(Gray, img))
 end
 
-end # module
+end
+
+
+
+
+
+
+
+
